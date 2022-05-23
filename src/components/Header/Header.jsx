4d95afed4bc7bada822c49d6bee233aaa19a1573
Andrija <kostic.andrija00@gmail.com>
2022-05-23 10:13:28 +0200

    added href to header link

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,7 +5,7 @@ import styles from './Header.module.css';
 const Header = () => {
   return (
     <header className={styles.header}>
-        <p>This is react project by freecodecamp that I did and you can find code on my github <a href="#">here</a>.</p>
+        <p>This is react project by freecodecamp that I did and you can find code on my github <a href="https://github.com/kosserin/react-lorem" target="_blank">here</a>.</p>
     </header>
   )
 }
